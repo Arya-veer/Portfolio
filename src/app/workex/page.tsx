922378e438a0713946c0706623c5d4ac67481296
Arya veer singh chauhan <aryaveer.chauhan@1mg.com>
2025-01-21 20:09:54 +0530

    Fix

--- src/app/workex/page.tsx
+++ src/app/workex/page.tsx
@@ -5,7 +5,7 @@ export default function WorkEx(){
     return (
         <div className="py-10 md:pr-3 px-7 md:pl-7 flex flex-col h-full gap-10">
             <div className="transparent md:pl-8 flex flex-col gap-2">
-                <p className="text-4xl text-center font-semibold font-mono text-three">WORK EXPERIENCE</p>
+                <p className="text-4xl md:text-left text-center font-semibold font-mono text-three">WORK EXPERIENCE</p>
             </div>
             <div className="flex flex-col gap-10">
                 {
